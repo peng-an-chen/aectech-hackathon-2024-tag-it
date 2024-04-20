4880abd6415e6029efb676555613298d86473d4c
Peng-An Chen <ChenPen@sbsit.com>
2024-04-20 18:26:59 +0200

    test_deserialization

--- src/test.py
+++ src/test.py
@@ -1,11 +1,31 @@
 import json
 from pypdf import PdfReader
+from pypdf.generic import IndirectObject, BooleanObject, ArrayObject
 from annotations import *
 
 pdf_directory = "C://GitLab//aectech-hackathon-2024-tag-it//sample"
 src_directory = "C://GitLab//aectech-hackathon-2024-tag-it//src"
 json_directory = "C://GitLab//aectech-hackathon-2024-tag-it//json"
 reader = PdfReader(pdf_directory + "//A102_Annotated.pdf")
+
+def remove_indirect_objects(dictionary):
+    cleaned_dict = {}
+    for key, value in dictionary.items():
+        if isinstance(value, dict):
+            cleaned_dict[key] = remove_indirect_objects(value)
+        # elif isinstance(value, list):
+        #     cleaned_dict[key] = [remove_indirect_objects(item) for item in value]
+        elif not isinstance(value, (int, float, str, bool)):
+            if isinstance(value, IndirectObject):
+                print ("Indirect Object: ", key, value)
+            elif isinstance(value, BooleanObject):
+                print ("Boolean Object: ", key, value)
+            elif isinstance(value, ArrayObject):
+                print ("Array Object: ", key, value)
+            cleaned_dict[key] = None
+        else:
+            cleaned_dict[key] = value
+    return cleaned_dict
 
 # def extract_line_to_json(line_object):
 #     line_json = {}
@@ -18,6 +38,7 @@ reader = PdfReader(pdf_directory + "//A102_Annotated.pdf")
 
 number_of_pages = len(reader.pages)
 
+annotations_original = []
 annotations_export = []
 
 for page in reader.pages:
@@ -26,35 +47,41 @@ for page in reader.pages:
         for annot in page["/Annots"]:
             obj = annot.get_object()
             subtype = annot.get_object()["/Subtype"]
-            print (subtype)
-            print (obj)
-            x1, y1, x2, y2 = obj["/Rect"]
-            location = [[float(x1), float(y1)], [float(x2), float(y2)]]
-            if subtype == "/Text":
-                annotation = {"subtype": obj["/Subtype"], "content": obj["/Contents"]}
-                annotations_export.append(annotation)
-            elif subtype == "/Line":
-                line_annotation = LineAnnotation(obj)
-                annotations_export.append(line_annotation.to_json())
-            elif subtype == "/FreeText":
-                freetext_annotation = FreeTextAnnotation(obj)
-                annotations_export.append(freetext_annotation.to_json())
-            elif subtype == "/Highlight":
-                    coords = annot.get_object()["/QuadPoints"]
-                    x1, y1, x2, y2, x3, y3, x4, y4 = coords
-                    annotation = {"subtype": obj["/Subtype"], "content": obj["/Contents"],
-                                  # "quad_points": [[x1, y1], [x2, y2], [x3, y3], [x4, y4]]
-                                  }
-                    annotations_export.append(annotation)
-            else:
-                annotation = {"subtype": obj["/Subtype"], "location": location}
+            print (obj, '\n')
+            json_dict_original = {key: value for key, value in obj.items()}
+            print (json_dict_original, '\n')
+            json_dict = remove_indirect_objects(json_dict_original)
+            # print ("removed", json_dict, '\n')
+
+
+            annotations_original
+            annotations_export.append(json_dict)
+            # print (annotations_export)
+            # x1, y1, x2, y2 = obj["/Rect"]
+            # location = [[float(x1), float(y1)], [float(x2), float(y2)]]
+            # if subtype == "/Text":
+            #     annotation = {"subtype": obj["/Subtype"], "content": obj["/Contents"]}
+            #     annotations_export.append(annotation)
+            # elif subtype == "/Line":
+            #     line_annotation = LineAnnotation(obj)
+            #     annotations_export.append(line_annotation.to_json())
+            # elif subtype == "/FreeText":
+            #     freetext_annotation = FreeTextAnnotation(obj)
+            #     annotations_export.append(freetext_annotation.to_json())
+            # elif subtype == "/Highlight":
+            #         coords = annot.get_object()["/QuadPoints"]
+            #         x1, y1, x2, y2, x3, y3, x4, y4 = coords
+            #         annotation = {"subtype": obj["/Subtype"], "content": obj["/Contents"],
+            #                       # "quad_points": [[x1, y1], [x2, y2], [x3, y3], [x4, y4]]
+            #                       }
+            #         annotations_export.append(annotation)
+            # else:
+            #     annotation = {"subtype": obj["/Subtype"], "location": location}
                 # print("Annotation type not supported: ", obj["/Subtype"])
             # print(annotation)
 
-# print("Number of annotations to export: ", len(annotations_export))
 # Serialize annotations_export
 annotations_export_json = json.dumps(annotations_export, indent=4)
-
 save_path = json_directory + "//annotations.json"
 # Write to annotations.json file
 with open(save_path, 'w') as f:
