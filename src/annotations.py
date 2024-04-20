0a225ba983e1d76a8dacbc346fe17bd858676ac9
Peng-An Chen <ChenPen@sbsit.com>
2024-04-20 19:20:19 +0200

    add_objects

--- src/annotations.py
+++ src/annotations.py
@@ -1,20 +1,24 @@
-__all__ = ["FreeTextAnnotation", "LineAnnotation"]
+__all__ = ["FreeTextAnnotation", "LineAnnotation", "PolylineAnnotation", "PolygonAnnotation", "CircleAnnotation", "InkAnnotation"]
 
 class BaseAnnotation(object):
     def __init__(self, pypdf_obj):
         self.date = pypdf_obj['/CreationDate']
         self.subtype = pypdf_obj['/Subtype']
         self.author = pypdf_obj['/T']
-        self.bounding_box = [[pypdf_obj['/Rect'][0], pypdf_obj['/Rect'][1]],
-                             [pypdf_obj['/Rect'][2], pypdf_obj['/Rect'][3]]]
+        self.color = pypdf_obj['/C']
+        self.bounding_box = [[pypdf_obj['/Rect'][i], pypdf_obj['/Rect'][i + 1]] for i in range(0, len(pypdf_obj['/Rect']), 2)]
+        self.contents = pypdf_obj['/Contents'] if '/Contents' in pypdf_obj else None
 
     def to_json(self):
         return {
-            "author": self.author,
+            "date": self.date,
             "subtype": self.subtype,
+            "author": self.author,
+            "color": self.color,
             "bounding_box": self.bounding_box,
-            "date": self.date
+            "contents": self.contents
         }
+
 
 class FreeTextAnnotation(BaseAnnotation):
     def __init__(self, pypdf_obj):
@@ -26,6 +30,7 @@ class FreeTextAnnotation(BaseAnnotation):
         json_dict["contents"] = self.contents
         return json_dict
     
+
 class LineAnnotation(BaseAnnotation):
     def __init__(self, pypdf_obj):
         super().__init__(pypdf_obj)
@@ -36,4 +41,60 @@ class LineAnnotation(BaseAnnotation):
         json_dict = super().to_json()
         json_dict["location"] = self.location
         json_dict["arrow_type"] = self.arrow_type
+        return json_dict
+    
+
+class PolylineAnnotation(BaseAnnotation):
+    def __init__(self, pypdf_obj):
+        super().__init__(pypdf_obj)
+        self.vertices = pypdf_obj['/Vertices']
+        self.interior_color = pypdf_obj['/IC']
+    
+    def to_json(self):
+        json_dict = super().to_json()
+        json_dict["vertices"] = [[self.vertices[i], self.vertices[i + 1]] for i in range(0, len(self.vertices), 2)]
+        json_dict["interior_color"] = self.interior_color
+        return json_dict
+    
+
+class PolygonAnnotation(BaseAnnotation):
+    def __init__(self, pypdf_obj):
+        super().__init__(pypdf_obj)
+        self.vertices = pypdf_obj['/Vertices']
+    
+    def to_json(self):
+        json_dict = super().to_json()
+        json_dict["vertices"] = [[self.vertices[i], self.vertices[i + 1]] for i in range(0, len(self.vertices), 2)]
+        return json_dict
+
+
+class CircleAnnotation(BaseAnnotation):
+    def __init__(self, pypdf_obj):
+        super().__init__(pypdf_obj)
+        self.subject = pypdf_obj['/Subj']
+        self.radius = pypdf_obj['/RD']
+    
+    def to_json(self):
+        json_dict = super().to_json()
+        json_dict["subject"] = self.subject
+        json_dict["radius"] = self.radius
+        return json_dict
+
+
+class InkAnnotation(BaseAnnotation):
+    def __init__(self, pypdf_obj):
+        super().__init__(pypdf_obj)
+        self.subject = pypdf_obj['/Subj']
+        self.ink_list = pypdf_obj['/InkList'][0]
+        self.border_width = pypdf_obj['/BS']['/W']
+        self.border_style = pypdf_obj['/BS']['/S']
+        self.border_type = pypdf_obj['/BS']['/Type']
+    
+    def to_json(self):
+        json_dict = super().to_json()
+        json_dict["subject"] = self.subject
+        json_dict["border_width"] = self.border_width
+        json_dict["border_style"] = self.border_style
+        json_dict["border_type"] = self.border_type
+        json_dict["ink_list"] = [[self.ink_list[i], self.ink_list[i + 1]] for i in range(0, len(self.ink_list), 2)]
         return json_dict
--- src/test.py
+++ src/test.py
@@ -8,24 +8,24 @@ src_directory = "C://GitLab//aectech-hackathon-2024-tag-it//src"
 json_directory = "C://GitLab//aectech-hackathon-2024-tag-it//json"
 reader = PdfReader(pdf_directory + "//A102_Annotated.pdf")
 
-def remove_indirect_objects(dictionary):
-    cleaned_dict = {}
-    for key, value in dictionary.items():
-        if isinstance(value, dict):
-            cleaned_dict[key] = remove_indirect_objects(value)
-        # elif isinstance(value, list):
-        #     cleaned_dict[key] = [remove_indirect_objects(item) for item in value]
-        elif not isinstance(value, (int, float, str, bool)):
-            if isinstance(value, IndirectObject):
-                print ("Indirect Object: ", key, value)
-            elif isinstance(value, BooleanObject):
-                print ("Boolean Object: ", key, value)
-            elif isinstance(value, ArrayObject):
-                print ("Array Object: ", key, value)
-            cleaned_dict[key] = None
-        else:
-            cleaned_dict[key] = value
-    return cleaned_dict
+# def remove_indirect_objects(dictionary):
+#     cleaned_dict = {}
+#     for key, value in dictionary.items():
+#         if isinstance(value, dict):
+#             cleaned_dict[key] = remove_indirect_objects(value)
+#         # elif isinstance(value, list):
+#         #     cleaned_dict[key] = [remove_indirect_objects(item) for item in value]
+#         elif not isinstance(value, (int, float, str, bool)):
+#             if isinstance(value, IndirectObject):
+#                 print ("Indirect Object: ", key, value)
+#             elif isinstance(value, BooleanObject):
+#                 print ("Boolean Object: ", key, value)
+#             elif isinstance(value, ArrayObject):
+#                 print ("Array Object: ", key, value)
+#             cleaned_dict[key] = None
+#         else:
+#             cleaned_dict[key] = value
+#     return cleaned_dict
 
 # def extract_line_to_json(line_object):
 #     line_json = {}
@@ -38,33 +38,40 @@ def remove_indirect_objects(dictionary):
 
 number_of_pages = len(reader.pages)
 
-annotations_original = []
 annotations_export = []
 
 for page in reader.pages:
     if "/Annots" in page:
-        # print("Number of annotations on page: ", len(page["/Annots"]))
         for annot in page["/Annots"]:
             obj = annot.get_object()
             subtype = annot.get_object()["/Subtype"]
+            print (subtype)
             print (obj, '\n')
-            json_dict_original = {key: value for key, value in obj.items()}
-            print (json_dict_original, '\n')
-            json_dict = remove_indirect_objects(json_dict_original)
-            # print ("removed", json_dict, '\n')
+            # json_dict = {key: value for key, value in obj.items()}
+            # print (json_dict, '\n')
 
+            if subtype == "/FreeText":
+                freetext_annotation = FreeTextAnnotation(obj)
+                annotations_export.append(freetext_annotation.to_json())
+            elif subtype == "/Line":
+                line_annotation = LineAnnotation(obj)
+                annotations_export.append(line_annotation.to_json())
+            elif subtype == "/PolyLine":
+                polyline_annotation = PolylineAnnotation(obj)
+                annotations_export.append(polyline_annotation.to_json())
+            elif subtype == "/Polygon":
+                polygon_annotation = PolygonAnnotation(obj)
+                annotations_export.append(polygon_annotation.to_json())
+            elif subtype == "/Circle":
+                circle_annotation = CircleAnnotation(obj)
+                annotations_export.append(circle_annotation.to_json())
+            elif subtype == "/Ink":
+                ink_annotation = InkAnnotation(obj)
+                annotations_export.append(ink_annotation.to_json())
 
-            annotations_original
-            annotations_export.append(json_dict)
-            # print (annotations_export)
-            # x1, y1, x2, y2 = obj["/Rect"]
-            # location = [[float(x1), float(y1)], [float(x2), float(y2)]]
             # if subtype == "/Text":
             #     annotation = {"subtype": obj["/Subtype"], "content": obj["/Contents"]}
             #     annotations_export.append(annotation)
-            # elif subtype == "/Line":
-            #     line_annotation = LineAnnotation(obj)
-            #     annotations_export.append(line_annotation.to_json())
             # elif subtype == "/FreeText":
             #     freetext_annotation = FreeTextAnnotation(obj)
             #     annotations_export.append(freetext_annotation.to_json())
@@ -83,9 +90,7 @@ for page in reader.pages:
 # Serialize annotations_export
 annotations_export_json = json.dumps(annotations_export, indent=4)
 save_path = json_directory + "//annotations.json"
+
 # Write to annotations.json file
 with open(save_path, 'w') as f:
     f.write(annotations_export_json)
-
-# with open('annotations.json', 'w') as f:
-#     f.write(annotations_export_json)
